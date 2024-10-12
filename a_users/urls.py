616303bacb56aeea46066005b34515fe65023b27
HerrWolf <webmasterp2m@gmail.com>
2024-10-12 16:06:51 -0700

    fix profile edit error

--- a_users/urls.py
+++ a_users/urls.py
@@ -3,7 +3,7 @@ from .views import *
 
 urlpatterns = [
     path('profile/', profile_view, name='profile'),
-    path('profile/<username>/', profile_view, name='userprofile'),
+    path('<username>/profile/', profile_view, name='userprofile'),
     path('profile/edit/', profile_edit_view, name='profile-edit'),
     path('profile/onboarding/', profile_edit_view, name='profile-onboarding'),
     path('profile/delete/', profile_delete_view, name='profile-delete'),
